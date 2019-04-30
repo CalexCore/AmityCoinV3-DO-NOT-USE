5f9beab7f1522a56c6689fdbeb840a687c2d44aa
Angry Wasp <angrywasp@live.com>
2019-04-30 16:58:39 +1000

    Change ASCII art message macro

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -1503,7 +1503,7 @@ namespace cryptonote
         main_message = "The daemon will start synchronizing with the network. This may take a long time to complete.";
 
       //Forker: change ascii art if you want
-      MGINFO_BLUE(ENDL 
+      MGUSER_BLUE(ENDL 
       << R"(════════════════════════════════════════════════════════════════════════════════)" << ENDL
       << R"(██╗   █████╗ ███╗   ███╗██╗████████╗██╗   ██╗ ██████╗ ██████╗ ██╗███╗   ██╗  ██╗)" << ENDL
       << R"(╚██╗ ██╔══██╗████╗ ████║██║╚══██╔══╝╚██╗ ██╔╝██╔════╝██╔═══██╗██║████╗  ██║ ██╔╝)" << ENDL
