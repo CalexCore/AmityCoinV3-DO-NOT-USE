7a1878e5766473141b3b990d636f06783823feb3
hooftly <mhooft@tf-dev.org>
2019-07-08 20:40:04 -0600

    ensure no NULL is passed to memcpy
    NULL is valid when size is 0, but memcpy uses nonnull attributes.

--- src/crypto/keccak.c
+++ src/crypto/keccak.c
@@ -117,6 +117,8 @@ void keccak(const uint8_t *in, size_t inlen, uint8_t *md, int mdlen)
     }
 
     memcpy(temp, in, inlen);
+    if (inlen > 0)
+      memcpy(temp, in, inlen);
     temp[inlen++] = 1;
     memset(temp + inlen, 0, rsiz - inlen);
     temp[rsiz - 1] |= 0x80;
